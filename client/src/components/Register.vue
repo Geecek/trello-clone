2dceee1e844749259321f6a7bb44f08b0072d95c
Geecek <gicalamaciej@gmail.com>
2018-06-13 11:23:15 +0200

    Add registration functionality

---
+++ client/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="example@email.com" v-model="credentials.email">
+    <input type="password" name="password" placeholder="password" v-model="credentials.password">
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      credentials: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register(this.credentials)
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'test message'
+    message: `Hello ${req.body.email}!`
   })
 })
 

045d5bb6e7592515005254f46ff4cbbef5b3a6e3
Geecek <gicalamaciej@gmail.com>
2018-11-08 20:09:09 +0100

    Add boards store module

---
+++ src/store/modules/boards.js
@@ -0,0 +1,19 @@
+const state = {
+  boardTitles: [
+    { title: 'Social life' },
+    { title: 'Workout plan' }
+  ]
+}
+
+const actions = {
+}
+
+const mutations = {
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+}

e07b54f17c28542d2308051b3af3863ba28dfcd8
Geecek <gicalamaciej@gmail.com>
2018-11-09 21:44:41 +0100

    Add AddBoard component

---
+++ src/components/AddBoard.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="new-board">
+    <v-btn
+      class="absolute"
+      :class="{visible: editing, invisible: !editing}"
+      @click="newBoard"
+    ><span class="plus">+</span> Add new board</v-btn>
+    <v-form
+      class="absolute"
+      :class="{visible: !editing, invisible: editing}"
+    >
+      <v-text-field
+        ref="text"
+        v-model="title"
+        :counter="16"
+        placeholder="Enter board title..."
+        solo
+        required
+      ></v-text-field>
+      <div class="form-menu">
+        <v-btn @click="addNewBoard">Add board</v-btn>
+        <v-btn
+          class="escape"
+          @click="changeView"
+        >exit</v-btn>
+      </div>
+    </v-form>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      editing: true
+    }
+  },
+  methods: {
+    changeView () {
+      this.editing = !this.editing
+      this.title = ''
+    },
+    newBoard () {
+      this.changeView()
+      this.$nextTick(() => this.$refs.text.focus())
+    },
+    addNewboard () {
+      if (this.title) {
+        this.$store.dispatch('boards/pushBoard', { title: this.title })
+        this.changeView()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.new-board {
+  max-width: 180px;
+}
+
+.plus {
+  font-size: 1.8em;
+  margin-right: 0.6em
+}
+
+.visible {
+  opacity: 1;
+}
+
+.invisible {
+  pointer-events: none;
+  opacity: 0;
+}
+
+.absolute {
+  position: absolute;
+}
+</style>

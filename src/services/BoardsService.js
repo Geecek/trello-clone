ce4b0ff458e8191e03a7d64d2f6ba40fb73edf13
Geecek <gicalamaciej@gmail.com>
2018-11-20 11:41:48 +0100

    Add posting new boards

--- src/services/BoardsService.js
+++ src/services/BoardsService.js
@@ -3,5 +3,8 @@ import Api from '@/services/Api'
 export default {
   get () {
     return Api().get('/boards')
+  },
+  post (board) {
+    return Api().post('/boards', board)
   }
 }
--- src/store/modules/boards.js
+++ src/store/modules/boards.js
@@ -9,12 +9,20 @@ const actions = {
     BoardsService.get().then((response) => {
       context.commit('setBoards', { boards: response.data.boards })
     })
+  },
+  pushBoard (context, board) {
+    BoardsService.post(board).then((response) => {
+      context.commit('pushBoard', { title: response.data.title })
+    })
   }
 }
 
 const mutations = {
   setBoards (state, { boards }) {
     state.boardTitles = boards
+  },
+  pushBoard (state, board) {
+    state.boardTitles = state.boardTitles.concat([board])
   }
 }
 

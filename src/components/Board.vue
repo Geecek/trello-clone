3261a791f6056119d6e4b6294e990ad6908bc660
Geecek <gicalamaciej@gmail.com>
2018-11-21 13:24:15 +0100

    Add cards fetching on mount

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -28,6 +28,7 @@ export default {
   },
   mounted () {
     this.$store.dispatch('lists/fetchLists', { parent: this.$route.params.id })
+    this.$store.dispatch('cards/fetchCards')
   }
 }
 </script>
--- src/store/modules/cards.js
+++ src/store/modules/cards.js
@@ -1,48 +1,20 @@
-import Vue from 'vue'
+import CardsService from '@/services/CardsService'
 
 const state = {
-  cardTitles: {
-    Uni: [
-      'SO'
-    ],
-    Shopping: [
-      'Bread',
-      'Cheese',
-      'Apples'
-    ],
-    Trello: [
-
-    ]
-  }
+  cardTitles: []
 }
 
 const actions = {
-  pushList (context, title) {
-    context.commit('pushList', title)
-  },
-  updateCard (context, payload) {
-    context.commit('updateCard', payload)
-  },
-  deleteCard (context, payload) {
-    context.commit('deleteCard', payload)
-  },
-  addCard (context, payload) {
-    context.commit('addCard', payload)
+  fetchCards (context) {
+    CardsService.get().then((response) => {
+      context.commit('setCards', response.data)
+    })
   }
 }
 
 const mutations = {
-  pushList (state, { title }) {
-    Vue.set(state.cardTitles, title, [''])
-  },
-  updateCard (state, { parent, title, newTitle }) {
-    state.cardTitles[parent] = state.cardTitles[parent].map(el => el === title ? newTitle : el)
-  },
-  deleteCard (state, { parent, title }) {
-    state.cardTitles[parent] = state.cardTitles[parent].filter(el => el !== title)
-  },
-  addCard (state, { title }) {
-    state.cardTitles[title].push('')
+  setCards (state, { todos }) {
+    state.cardTitles = todos
   }
 }
 
